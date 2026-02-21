e713428a1668fa9aab2b05a591d8f5f2d446c468
salamaa12 <yousefahmedsalama8@gmail.com>
2026-02-21 23:59:17 +0200

    Update UpdatePatientProfileDto.cs

--- DTOs/UpdatePatientProfileDto.cs
+++ DTOs/UpdatePatientProfileDto.cs
@@ -1,9 +1,9 @@
 ﻿namespace Yoser_API.DTOs
 {
     public class UpdatePatientProfileDto
-    {
-        public string MedicalCondition { get; set; } = string.Empty;
-        public int Age { get; set; }
-        public string EmergencyContact { get; set; } = string.Empty;
-    }
+{
+    public string? MedicalCondition { get; set; }
+    public int? Age { get; set; }
+    public string? EmergencyContact { get; set; }
+}
 }
